03869c451d01b444161afb323c7b7b4eeedd2c7d
Rafael S Magalhaes <rafaelsmag@gmail.com>
2018-04-26 19:43:40 -0300

    Changed folder location of cardHeader

---
+++ src/modules/home/components/CardHeader.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { func, string, arrayOf, shape } from 'prop-types'
+import { Div } from 'glamorous'
+import { Metrics, Fonts } from '../../../config/constants'
+import SmallAvatarRow from '../../shared/components/rows/SmallAvatarRow'
+import Tags from '../../shared/components/Tags'
+
+const CardHeader = ({title, tags, onClick, author}) => (
+  <Div css={styles.cardHeader} onClick={onClick}>
+    <Div css={styles.title}>{title}</Div>
+    {
+      tags &&
+      <Tags tags={tags} />
+    }
+    {
+      author &&
+      <SmallAvatarRow title={author.name} image={author.image}/>
+    }
+  </Div>
+)
+
+CardHeader.defaultProps = {
+  onClick: () => {},
+  title: '',
+  tags: undefined,
+  author: undefined
+}
+
+CardHeader.propTypes = {
+  onClick: func,
+  title: string,
+  tags: arrayOf(
+    shape({
+      name: string.isRequired,
+      _id: string
+    })
+  ),
+  author: shape({
+    name: string,
+    image: string
+  })
+}
+
+const styles = {
+  cardHeader: {
+    padding: Metrics.smallSpacing
+    // borderBottom: `${Metrics.borderWidth} ${Colors.darkDivider} solid`
+  },
+  title: {
+    paddingBottom: Metrics.xxsSpacing,
+    paddingTop: Metrics.xxsSpacing,
+    ...Fonts.style.h5
+  }
+}
+
+export default CardHeader
