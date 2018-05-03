95207b57effc69e5ecdd71effccbb21c4f253716
Rafael S Magalhaes <rafaelsmag@gmail.com>
2018-05-03 20:13:01 -0300

    Adding tests to FeedCard component

---
+++ src/__test__/home/components/FeedCard.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import FeedCard from '../../../modules/home/components/FeedCard'
+
+const props = {
+  post: {
+    _id: '1',
+    title: 'Lorem ipsum dolor sit amet',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque, risus sit amet blandit molestie, nunc tellus elementum sem, eu condimentum odio nibh quis arcu. Etiam consectetur libero et lectus ultricies, sed convallis nisl rutrum. Nunc a dolor quis tortor congue facilisis quis quis magna. Nulla facilisi. Nulla cursus tellus felis, varius venenatis sapien vulputate nec. Vestibulum vel lacus dignissim risus iaculis malesuada. Sed accumsan sem in mi ornare luctus. Donec nec odio porttitor, sollicitudin mi non, rutrum justo. Pellentesque eu ultricies erat, et posuere sem. Ut ac mi vitae ipsum rutrum posuere. Nam dignissim pellentesque ipsum, et semper nibh pellentesque nec. Etiam eleifend, libero sit amet ultricies ullamcorper, urna augue dictum odio, id ultricies ligula odio nec ex.',
+    images: ['https://static.pexels.com/photos/371633/pexels-photo-371633.jpeg'],
+    user: {
+      image: 'https://www.shareicon.net/data/128x128/2016/07/26/802026_man_512x512.png',
+      name: 'Steve Sanders'
+    },
+    tags: [{_id: '1', name: 'Sobremesas'}, {_id: '2', name: 'Bolo'}]
+  }
+}
+
+describe('FeedCard Component', () => {
+  const feedCard = shallow(<FeedCard {...props} />)
+  it('should redenders properly', () => {
+    expect(feedCard).toMatchSnapshot()
+  })
+})
